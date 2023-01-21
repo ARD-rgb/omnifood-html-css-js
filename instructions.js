c513241adad169ef40e04d51e84442739eb0feee
ard-RGB <ardrgb@gmail.com>
2023-01-21 15:01:46 +0530

    Initial commit

---
+++ instructions.js
@@ -0,0 +1,131 @@
+// PAR1
+// 1.PUTTING IMAGE ON RIGHT AND TEXT ON LEFT-CHECK
+// 2 Using max width and margin trick with rem unit instead of pixels-CHECK
+// 4. To hero section[section-hero] get the #fdf2e9 background color-CHECK
+// 5. Add spacing between section and "two grid boxes" -CHECK
+// 6.Format btns[btn] that  are anchro/link
+// 7.Styling btn-full and btn--outline -CHECK
+// 8.Don't want to use flexbox or grid so add a helper class to add margin right to the btn--outline-CHECK
+// 9.
+
+// PART2(Lecture:Building hero P3)
+// 1.Get the rubik font and use it body, for fall back use san serif -CHECK
+// 2.Make buttons 600 bold CHECK
+// 3.Put multiple images and a simpe 4 word text side by side and add color to first word
+// 4.Formatting tedious small components:
+/*
+make 250,000+ same color as our Primary: #e67e22 and also check color  contrast ration for it
+remove margin from side of last img
+to get squared round customer  photots
+add space above delivered meals
+put both of them side by side
+Putting customer images and text side by side
+*/
+
+// PART 3:(110.Building the header)
+// Logo on left and nav on right,wrap  it inside a container-CHECK
+//Format the logo, make it small
+// Because we want header to be sticky later
+// push logo and nav to opposite side
+// Format the logo, make it small
+
+// PART 4:[111.Building the Navigation].Put the links horizonally side by side
+// 1.Create the html structure for those links.
+// 2.Adding call to action section to our last element of cta
+//3.Small changes:
+// to align all links horizonatlly which are right top of page
+// small change 2: format individual links
+// we added .main-nav-link.nav-cta:link to apply  color: #fff;(0,3,0) not   color: #333;(0,2,0)
+
+// PART 5:REUSABLE GRID CREATION
+// KEY CHANGES:HOW IT WORKS SECTION
+// notthing here
+// =====================================================
+// PART 6:113.HOW IT WORKS SECTION
+// KEY WORK:ZIG ZAG PATTERN OF TEXT AND IMAGES AND HEADINGS */
+//  STYLES
+// SMALL CHANGESŚ:
+// 1.FROM  BUILDING ZIG ZAG PATTER: .heading-primary,.heading-secondary
+// 2.FROM  BUILDING ZIG ZAG PATTER: heading-secondary, subheading
+
+// CURRENT
+
+// 4.Adding circle behind all android phone image
+// 5.Adding second small circle behind all android phone image
+// 6.Adding logos to the section
+// 6.a.Styling logos
+// 1.structuring and resizing images smaller
+// 2.making the images grey
+
+// PART 7:BUILDING THE MEAL SECTION
+//ionicons: fir eat star
+// stroke,fill,color check for various
+// formatting strong element
+// formattting Vegetarian,.tag
+
+// tag--vegetarain
+// tag--vegan
+// tag--paleo
+// /* adding hover effect on our cards */
+// /* center "MEAL" subheading */
+// /* styling "see all reciepes"  */
+// /* setting  "see all reciepes" at center */
+// /* putting  "see all reciepes" little bit below */
+// /* align the icon and the list items */
+
+// PART 8:BUILDING THE TESTIMONIAL SECTION PART -1
+// PART 8:BUILDING THE TESTIMONIAL SECTION PART -2
+// removing annoying tiny space between images,images are inline elements by defautl
+// put images in 3 columns by creating new grid(step1)
+// put images in 3 columns by creating new grid(step2)
+// put images in 3 columns by creating new grid(step3)
+// to stop wall of images to jump when squeezed the browser
+// adding effect on our image wall when hovered step1
+// adding effect on our image wall when hovered step2
+// adding effect on our image wall when hovered,prevent overflow step3
+
+// PART 9:BUILDING PRICING  SECTION PART 1
+//##MICRO/FIDGET CHANGES:
+// html:plan1 plan2
+
+// PART 9:BUILDING PRICING  SECTION PART 2
+//##MICRO/FIDGET CHANGES:
+// Image are block level,so they occupier entire width of contaier to make them small
+// To put 1st cards in sensible location
+// add cross icon from ion icon and add class
+// adding ribbon to the complete plan using psuedo class and absolute positioning and rotation and overflow part1
+// adding ribbon to the complete plan using psuedo class and absolute positioning and rotation and overflow part2
+// to keep 9.6rem margin-bottom only on last child
+// PART 10:BUILDING FEATURES PART
+// adding circles around the 4 icons
+// Remaining piece of content
+
+// PART 10:CTA[CALL TO ACTION] PART 1
+// PART 11:CTA[CALL TO ACTION] PART 2
+//########Micro Changes:
+// Adding label and input inside a form
+// adding border around input filed when clicked on label, using for and in element
+// tickbox and number box, we can add lable to each one of them
+// input fields are inline but when we incapsulate them inside a div they become block level
+// putting our  input  and labels in a 2 col grid
+// PART 10:CTA[CALL TO ACTION] PART 2 [css formatting elements]
+// add btn to the form
+
+// PART 10:CTA[CALL TO ACTION] PART 3[styling]
+// removing  all ugly borders and adding cool once all over the web page
+
+// PART 11:BUILDING FOOTER:127 HTML
+//########Micro Changes:
+// gona implment later
+// tel and mailto helpful on phon
+// add social inst fb twtitter icons
+
+// PART 11:BUILDING FOOTER:127 CSS
+
+// imp trick, many don't know:
+// to push down copyright text 45px down p1
+// to push down copyright text 45px down p2
+
+// formatting footer 5 columns to add space between last three
+// adding space between previous section and footer
+// linking to  making it in between
